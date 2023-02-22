231bae47e7a0ecca441ad21eb83e947395e47d20
fabienhinault <fabienhinault@users.noreply.github.com>
2023-02-22 14:59:35 +0100

    test toNext

--- test.m12.js
+++ test.m12.js
@@ -6,3 +6,28 @@ describe('Array', function () {
         });
     });
 });
+
+describe('Table', function() {
+    describe('toNext', function() {
+        it('', function () {
+            chai.assert.equal(toNext(""), "I");
+            chai.assert.equal(toNext("I"), "M");
+            chai.assert.equal(toNext("M"), "II");
+            chai.assert.equal(toNext("II"), "IM");
+            chai.assert.equal(toNext("IM"), "MI");
+            chai.assert.equal(toNext("MI"), "MM");
+            chai.assert.equal(toNext("MM"), "III");
+            chai.assert.equal(toNext("III"), "IIM");
+            chai.assert.equal(toNext("IIM"), "IMI");
+            chai.assert.equal(toNext("IMI"), "IMM");
+            chai.assert.equal(toNext("IMM"), "MII");
+            chai.assert.equal(toNext("MII"), "MIM");
+            chai.assert.equal(toNext("MIM"), "MMI");
+            chai.assert.equal(toNext("MMI"), "MMM");
+            chai.assert.equal(toNext("MMM"), "IIII");
+            chai.assert.equal(toNext("IIII"), "IIIM");
+            chai.assert.equal(toNext("MMMI"), "MMMM");
+            chai.assert.equal(toNext("MMMM"), "IIIII");
+        });
+    });
+});
