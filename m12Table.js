fecf1266c9537521f06040c0a0d1d6711a678847
Fabien Hinault <fabien.hinault@free.fr>
2023-03-03 22:38:31 +0100

    json

--- m12Table.js
+++ m12Table.js
@@ -4,7 +4,8 @@ function toNext(str) {
 	// all strings from "IIII..." to "IMIM..." are dirty
         return "IM".repeat(str.length + 1).substring(0, str.length + 1) ;
     } else {
-        return str.substring(0, lastI) + "M" + "I".repeat(str.length - lastI - 1);
+        return str.substring(0, lastI) + "M" + 
+            "IM".repeat(str.length).substring(0, str.length - lastI - 1);
     }
 }
 
@@ -38,42 +39,28 @@ function applyString(str, numbers) {
 const A = range(12, 0);
 let map = {};
 
-let db;
-const request = indexedDB.open("m12DB");
-request.onerror = (event) => {
-  console.error(`Database error: ${event.target.errorCode}`);
-};
-
-request.onsuccess = (event) => {
-  db = event.target.result;
-   const objectStore = db
-      .transaction("m12Store", "readwrite")
-      .objectStore("m12Store");
-    const start = Date.now();
-    for (const str of allMIs("M".repeat(21), 23)) {
-        console.log(getNameFromAction(str));
+function compute(startString, intMaxSize) {
+    for (const str of allMIs(startString, intMaxSize)) {
         if (isClean(str, 12)) {
+            let name = getNameFromAction(str); 
+            console.log(name);
             const res = applyString(str, A);
             let cycles = getCycles(res);
-            objectStore.add({
-                rawNumbers:res, 
-                miString: str,
-                cycles: cycles,
-                maxLengthCycle: Math.max.apply(null, cycles.map(c => c.length)),
-                invariants: getInvariants(cycles),
-                inverse: getPermutationInverseRaw(res)
-            });
+            if (map[res] === undefined){
+                map[res] = ({
+                    rawNumbers:res, 
+                    miString: str,
+                    name: name,
+                    cycles: cycles,
+                    maxLengthCycle: Math.max.apply(null, cycles.map(c => c.length)),
+                    invariants: getInvariants(cycles),
+                    inverse: getPermutationInverseRaw(res)
+                });
+            }
         }
     }
-    console.log(Date.now() - start);
-};
+}
+let start = Date.now();
+compute("", 23);
+console.log(Date.now() - start);
 
-request.onupgradeneeded = (event) => {
-  const db = event.target.result;
-
-  // Create an objectStore to hold information about our customers. We're
-  // going to use "ssn" as our key path because it's guaranteed to be
-  // unique - or at least that's what I was told during the kickoff meeting.
-  const objectStore = db.createObjectStore("m12Store", { keyPath: "rawNumbers" });
-
-};
