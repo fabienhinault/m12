d1bb29464f1e19ef21e6f03806fa33b66bceba79
Fabien Hinault <fabien.hinault@free.fr>
2025-04-22 20:00:59 +0200

    svg

--- m12.js
+++ m12.js
@@ -26,92 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const divNumbersFooter = document.querySelector('#numbers-footer');
     const divInfos = document.querySelector('#infos');
     const buttonI = document.querySelector('#I');
+    const imgI = document.querySelector('#I img');
     const buttonM = document.querySelector('#M');
-    const buttonPlus = document.querySelector('#plus');
-    const divShortcuts = document.querySelector('#shortcuts');
-    const buttonSaveShortcut = document.querySelector('#save_shortcut');
-    const inputShortcut = document.querySelector('#input_shortcut');
-    const inputShortcutName = document.querySelector('#input_shortcut_name');
 
     const buttonShuffle = document.querySelector('#shuffle');
-    const inputShuffle = document.querySelector('#input_shuffle');
-    const spanDefShortcut = document.querySelector('#def_shortcut');
-    const buttonReset = document.querySelector('#reset');
-    const buttonUndo = document.querySelector('#undo');
-    const buttonSolution = document.querySelector('#btn_solution');
-    const spanSolution = document.querySelector('#solution');
     const divTime = document.querySelector('#time');
     const divNumbers = document.querySelector('#numbers');
-    const buttons = [buttonShuffle, buttonReset, buttonUndo, buttonPlus];
 
-    function toggleOnAddShortcut() {
-        addingShortcut = true;
-        buttonI.onclick = plusI;
-        buttonM.onclick = plusM;
-        divShortcuts.childNodes.forEach(
-            (btn, i) => {
-                btn.onclick = () => model.applyShortcutToShortcut(i);
-            });
-        buttons.forEach(b => b.disabled = true);
-        spanDefShortcut.style["visibility"] = "visible";
-    }
-
-    function toggleOffAddShortcut() {
-        addingShortcut = false;
-        buttonI.onclick = () => model.I();
-        buttonM.onclick = () => model.M();
-        buttons.forEach(b => b.disabled = false);
-        spanDefShortcut.style["visibility"] = "collapse";
-    }
-
-    function plusI() {
-        model.currentShortcut.add("I");
-    }
-
-    function plusM() {
-        model.currentShortcut.add("M");
-    }
-
-    function saveShortcut() {
-        model.saveCurrentShortcut(inputShortcutName.value);
-        toggleOffAddShortcut();
-    }
-function toggleSolution() {
-    showSolution = !showSolution;
-    if (showSolution) {
-        toggleOnSolution();
-        model.updateSolution(); 
-    } else {
-        spanSolution.innerHTML = '' 
-        toggleOffSolution();
-    }
-}    
-
-function toggleOffSolution() {
-    document.removeEventListener('solution changed', updateSpanSolution);
-}
-
-function updateSpanSolution(evt) {
-    spanSolution.innerHTML = evt.detail.solution;
-}
-
-function toggleOnSolution() {
-    document.addEventListener('solution changed', updateSpanSolution);
-}
 
 function initBigButton(button) {
     button.style.width = bigButtonWidth + "px";
+    button.firstChild.style.width = bigButtonWidth * 0.8 + "px";
     button.style.height = (2 * tileSidePx + 2) + "px";
+    button.firstChild.style.height = (2 * tileSidePx + 2) + "px";
 }
 
 function initControl(control) {
     control.style.width = tileSidePx + "px";
     control.style.height = tileSidePx + "px";
-}
-
-function initInput(input) {
-    input.style.width = inputSidePx + "px";
-    input.style.height = inputSidePx + "px";
 }
 
 function initDivTime() {
@@ -134,16 +66,8 @@ function initView() {
     initNumbersBorder(divNumbersFooter);
     initBigButton(buttonI);
     initBigButton(buttonM);
-    initControl(buttonPlus);
-    initControl(buttonUndo);
     initControl(buttonShuffle);
-    initControl(buttonSaveShortcut);
-    initControl(buttonReset);
-    initControl(buttonSolution);
 
-    initInput(inputShuffle);
-    initInput(inputShortcutName);
-    initInput(inputShortcut);
     initDivTime();
     initDivInfos();
 }
@@ -189,7 +113,8 @@ function initNumbersDiv(numbers, div) {
 function updateNumbersDiv(numbers, div) {
     numbers.toReversed().forEach(
         (number, index) => {
-            div.insertAdjacentElement('afterBegin', div.querySelector(`#tile-${number}`));
+            const tile = div.querySelector(`#tile-${number}`);
+            div.insertAdjacentElement('afterBegin', tile);
         }
     );
 }
@@ -204,7 +129,7 @@ function startChrono(evt) {
 }
 
 function shuffle() {
-    model.shuffle(Number(inputShuffle.value));
+    model.shuffle(100);
     document.addEventListener('numbers changed', startChrono);
 }
 
@@ -213,11 +138,6 @@ function shuffle() {
     buttonI.onclick = () => model.I();
     buttonM.onclick = () => model.M(); 
     buttonShuffle.onclick = shuffle;
-    buttonReset.onclick = () => model.reset();
-    buttonUndo.onclick = () => model.undo();
-    buttonSolution.onclick = () => toggleSolution();
-    buttonPlus.onclick = toggleOnAddShortcut;
-    buttonSaveShortcut.onclick = saveShortcut;
 
     document.addEventListener('keypress',
         evt => {
@@ -233,29 +153,14 @@ function shuffle() {
             if ('J' === key) {
                 model.M();
             }
-        });
+        }
+    );
+    model.reset();
     document.addEventListener('numbers changed',
         evt => {
             updateNumbersDiv(model.numbers, divNumbers);
-        });
-    document.addEventListener('shortcuts changed',
-        evt => {
-            divShortcuts.innerHTML = 
-                model.shortcuts
-                .map(shortcut => `<button> ${shortcut.name} </button>`)
-                .join(''); 
-            divShortcuts.childNodes.forEach(
-                (btn, i)  => { btn.onclick = () => model.applyString(model.shortcuts[i].action)});
-        });
-
-    document.addEventListener('currentShortcut changed',
-        evt => {
-            inputShortcut.value = model.currentShortcut.action;
-            inputShortcutName.value = model.currentShortcut.name;
-        });
-
-    model.reset();
-    toggleOffAddShortcut();
+        }
+    );
     initView();
     initNumbersDiv(model.numbers, divNumbers);
 });
