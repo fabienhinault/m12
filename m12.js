dc8499508640b9a8a81ab6becfce4cc9318c5db3
fabienhinault <fabienhinault@users.noreply.github.com>
2025-05-02 13:27:56 +0200

    updateNumbersDivM

--- m12.js
+++ m12.js
@@ -1,6 +1,6 @@
 // import {range, permute, getRandomInt, pick, makeMArray, makeMInvArray, getComplementModulo, getIsInverseLength, getMsInverseLength, getGroupInverse, getSolution} from 'libm12';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     const N = Number(new URL(window.location.toLocaleString()).searchParams.get('n'));
     let frame;
     if (N > 0 && N !== 12) {
@@ -31,7 +31,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const divTime = document.querySelector('#time');
     const divTitle = document.querySelector('#title');
     const divNumbers = document.querySelector('#numbers');
+    let tiles = [];
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function initBigButton(button) {
     button.style.width = bigButtonWidth + "px";
@@ -86,7 +90,7 @@ function createNumberDiv(i) {
     const div = document.createElement("div");
     div.style.width = tileSidePx + "px";
     div.style.backgroundColor = backColors[i - 1];
-    div.style.transition = `inset-inline-start ${transitionDurationMillis}ms`
+    div.style.transition = `left ${transitionDurationMillis}ms`
     div.className = "tile";
     return div;
 }
@@ -109,18 +113,59 @@ function initNumbersDiv(numbers, div) {
         if (i > model.N / 2) {
             subdiv.style.color = "white";
         }
+        tiles[i] = subdiv;
         div.appendChild(subdiv);
     }
 }
+async function updateNumbersDiv() {
+    const last = model.lasts.at(-1);
+    if (last === 'M') {
+        updateNumbersDivM(model.previousNumbers, model.numbers);
+    } else {
+        updateNumbersDivI(model.numbers);
+    }
+}
+ 
+async function updateNumbersDivI(numbers) {
+    const verticalTransitionDuration = 0.1 * transitionDurationMillis;
+    const horizontalTransitionDuration = 0.8 * transitionDurationMillis;
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        const tile = tiles[number];
+        tile.style.transition = `left ${horizontalTransitionDuration}ms, top ${verticalTransitionDuration}ms`;
+        tile.style.left = `${index * tileSideWithMargin}px`;
+    }
+}
 
-function updateNumbersDiv(numbers, div) {
-    numbers.forEach(
-        (number, index) => {
-            const tile = div.querySelector(`#tile-${number}`);
-            tile.style.insetInlineStart = `${index * tileSideWithMargin}px`;
-            tile.style.transition = `inset-inline-start ${transitionDurationMillis}ms`
+
+async function updateNumbersDivM(oldNumbers, numbers) {
+    const verticalTransitionDuration = 0.1 * transitionDurationMillis;
+    const horizontalTransitionDuration = 0.8 * transitionDurationMillis;
+    if (transitionDurationMillis >= 200) {
+        for (let index = 1; index < Math.floor(numbers.length / 2); index++) {
+            let number = oldNumbers[index];
+            const tile = tiles[number];
+            tile.style.transition = `left ${horizontalTransitionDuration}ms, top ${verticalTransitionDuration}ms`;
+            tile.style.top = `${tileSidePx / 2}px`;
         }
-    );
+        for (let index = Math.floor(numbers.length / 2); index < numbers.length; index++) {
+            let number = oldNumbers[index];
+            const tile = tiles[number];
+            tile.style.transition = `left ${horizontalTransitionDuration}ms, top ${verticalTransitionDuration}ms`;
+            tile.style.top = `${-tileSidePx / 2}px`;
+        }
+        await sleep(verticalTransitionDuration);
+    }
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        const tile = tiles[number];
+        tile.style.left = `${index * tileSideWithMargin}px`;
+    }
+    await sleep(horizontalTransitionDuration);
+    for (let number of numbers) {
+        const tile = tiles[number];
+        tile.style.top = 0;
+    }
 }
 
 function startChrono(evt) {
@@ -134,7 +179,7 @@ function startChrono(evt) {
 
 function shuffle() {
     model.shuffle(100);
-    document.addEventListener('numbers changed', startChrono);
+    updateNumbersDivI(model.numbers);
 }
 
 function updateTransitionDuration() {
@@ -144,22 +189,25 @@ function updateTransitionDuration() {
 
 function M() {
     updateTransitionDuration();
+    const oldNumbers = model.numbers;
     model.M();
+    updateNumbersDivM(oldNumbers, model.numbers);
 }
-
 
 function I() {
     updateTransitionDuration();
     model.I();
+    updateNumbersDivI(model.numbers);
 }
+
     document.documentElement.style.setProperty('--white', white);
     document.documentElement.style.setProperty('--blue', blue);
-    buttonI.onclick = () => I();
-    buttonM.onclick = () => M(); 
+    buttonI.onclick = async () => I();
+    buttonM.onclick = async () => M(); 
     buttonShuffle.onclick = shuffle;
 
     document.addEventListener('keypress',
-        evt => {
+        async evt => {
             const key = evt.key.toUpperCase();
             // M for right index
             if (['J', 'M'].includes(key)) {
@@ -173,11 +221,11 @@ function I() {
     );
     model.reset();
     document.addEventListener('numbers changed',
-        evt => {
+        async evt => {
             updateNumbersDiv(model.numbers, divNumbers);
         }
     );
     initView();
     initNumbersDiv(model.numbers, divNumbers);
-    updateNumbersDiv(model.numbers, divNumbers);
+    await updateNumbersDivI(model.numbers);
 });
